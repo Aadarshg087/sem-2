c469d0b336cc62245e54734e16b5eb54597463ec
Aadarshg087 <aadarshg087@gmail.com>
2022-03-01 22:23:00 +0530

    Code Formatted

--- Basics/Practice.cpp
+++ Basics/Practice.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main()
 {
     int a, b;
-    cin >> a; 
-    cout<<"Here is the value of a: "<<a<<endl;
+    cin >> a;
+    cout << "Here is the value of a: " << a << endl;
     cin >> b;
-    cout<<"Here is he value of b: "<<b<<endl;
-    
-    
+    cout << "Here is he value of b: " << b << endl;
+
     return 0;
 }
